8f86f4fbe4e06ba85b85cfdcf003145175c96af3
floumaster <dimaslubitsk@gmail.com>
2021-02-16 18:33:16 +0200

    проблема сверху кастомизации

--- Project/app/src/components/customize/uniqueBox/uniqueBox.js
+++ Project/app/src/components/customize/uniqueBox/uniqueBox.js
@@ -23,20 +23,30 @@ function UniqueBox(){
                         </div>
                     </div>
                     <div className="unique-customize-left-menu-img">
-                        <img src="https://manage.teakandtwine.com/storage/images/13161/cca02edfed4fef220442b86b8246a5d2.jpg" alt="logo"/>
+                        <img className="unique-customize-left-menu-img-main" src="https://manage.teakandtwine.com/storage/images/13161/cca02edfed4fef220442b86b8246a5d2.jpg" alt="logo"/>
                     </div>
                     <hr></hr>
                     <div className="unique-customize-left-menu-img-list">
-                    <a><img src="https://manage.teakandtwine.com/storage/images/1037/6eead4df9b3854b90a6eaeb773993d96.jpg" alt="logo"/></a>
-                    <a><img src="https://manage.teakandtwine.com/storage/images/13159/e6359ec22ae9bf8e3089e90c1ca3cc95.jpg" alt="logo"/></a>
-                    <a><img src="https://manage.teakandtwine.com/storage/images/13160/bbc8e15ffdd43a083fce4bfa1e85dcd0.jpg" alt="logo"/></a>
-                    <a><img src="https://manage.teakandtwine.com/storage/images/13161/3a3a4e8ef2517c88a7afb2e79e36c7e1.jpg" alt="logo"/></a>
+                    <a onClick={()=>{
+                        document.getElementsByClassName("unique-customize-left-menu-img-main")[0].src = document.getElementsByClassName("unique-customize-left-menu-img-img1")[0].src;
+                    }}><img className="unique-customize-left-menu-img-img1" src="https://manage.teakandtwine.com/storage/images/1037/934237552924f09c32cef031054cbf9f.jpg" alt="logo"/></a>
+                    <a onClick={()=>{
+                        document.getElementsByClassName("unique-customize-left-menu-img-main")[0].src = document.getElementsByClassName("unique-customize-left-menu-img-img2")[0].src;
+                    }}><img className="unique-customize-left-menu-img-img2" src="https://manage.teakandtwine.com/storage/images/13159/be774f5124296da97d9eee6f1c59a717.jpg" alt="logo"/></a>
+                    <a onClick={()=>{
+                        document.getElementsByClassName("unique-customize-left-menu-img-main")[0].src = document.getElementsByClassName("unique-customize-left-menu-img-img3")[0].src;
+                    }}><img className="unique-customize-left-menu-img-img3" src="https://manage.teakandtwine.com/storage/images/13160/929ec26d30fbdf19f77840533d25de73.jpg" alt="logo"/></a>
+                    <a onClick={()=>{
+                        document.getElementsByClassName("unique-customize-left-menu-img-main")[0].src = document.getElementsByClassName("unique-customize-left-menu-img-img4")[0].src;
+                    }}><img className="unique-customize-left-menu-img-img4" src="https://manage.teakandtwine.com/storage/images/13161/cca02edfed4fef220442b86b8246a5d2.jpg" alt="logo"/></a>
                     </div>
                 </div>
                 <div className="unique-customize-right">
-                    <h2>DETAILS</h2>
-                    <a>To get started on your custom design, click "customize" above! Custom gifts are shipped within 2-4 business days. Creating an account will allow you to save your designs so that you can come back to them later. If you have any questions about your design, don't hesitate to reach out to us at: hello@teakandtwine.com and we'll help you out right away!</a>
-                    <a className="a-btn">CUSTOMIZE</a> 
+                    <div>
+                        <h2>DETAILS</h2>
+                        <a>To get started on your custom design, click "customize" above! Custom gifts are shipped within 2-4 business days. Creating an account will allow you to save your designs so that you can come back to them later. If you have any questions about your design, don't hesitate to reach out to us at: hello@teakandtwine.com and we'll help you out right away!</a>
+                        <a className="a-btn">CUSTOMIZE</a> 
+                    </div>
                 </div>
             </div>
         </div>
